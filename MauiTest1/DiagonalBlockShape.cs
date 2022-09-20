a24e2c7d442b552fe184cc7bac3d703255a57ff5
Stephan Santos <stephan.pssantos@gmail.com>
2022-09-19 22:16:17 -0700

    Parameterize DiagonalBlockShape

--- MauiTest1/DiagonalBlockShape.cs
+++ MauiTest1/DiagonalBlockShape.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Layouts;
 
 namespace MauiTest1
 {
     public class DiagonalBlockShape : AbsoluteLayout
     {
-        private int BorderDepth = 3;
+        public static readonly BindableProperty BorderDepthProperty = 
+            BindableProperty.Create(nameof(BorderDepth), typeof(int), typeof(DiagonalBlockShape), 5);
         public DiagonalBlockShape()
         {
             Loaded += ResizeShape;
             SizeChanged += ResizeShape;
         }
 
+        public int BorderDepth 
+        { 
+            get { return (int)GetValue(BorderDepthProperty); }
+            set { SetValue(BorderDepthProperty, value); } 
+        }
+
         private void ResizeShape(object sender, EventArgs e)
         {
             AbsoluteLayout ThisObject = sender as AbsoluteLayout;
+
+            ThisObject.Children.Clear();
 
             Polygon shape1 = new()
             {
@@ -43,20 +53,12 @@ namespace MauiTest1
                 Fill = new SolidColorBrush(Color.FromArgb("#FFFFFF"))
             };
 
-            if (ThisObject.Children.Count == 2)
-            {
-                ThisObject.Children[0] = shape1;
-                ThisObject.Children[1] = shape2;
-            }
-            else if (ThisObject.Children.Count == 0)
-            {
-                ThisObject.Children.Add(shape1);
-                ThisObject.Children.Add(shape2);
-            }
+            ThisObject.Children.Add(shape1);
+            ThisObject.Children.Add(shape2);
 
-            AbsoluteLayout.SetLayoutFlags(shape1, Microsoft.Maui.Layouts.AbsoluteLayoutFlags.SizeProportional);
+            AbsoluteLayout.SetLayoutFlags(shape1, AbsoluteLayoutFlags.SizeProportional);
+            AbsoluteLayout.SetLayoutFlags(shape2, AbsoluteLayoutFlags.SizeProportional);
             AbsoluteLayout.SetLayoutBounds(shape1, new Rect(0, 0, 1, 1));
-            AbsoluteLayout.SetLayoutFlags(shape2, Microsoft.Maui.Layouts.AbsoluteLayoutFlags.SizeProportional);
             AbsoluteLayout.SetLayoutBounds(shape2, new Rect(0, 0, 1, 1));
         }
     }
