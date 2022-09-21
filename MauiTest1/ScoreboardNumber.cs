d15bba7736357e61a93d5376ef3eba7780e16b0e
Stephan Santos <stephan.pssantos@gmail.com>
2022-09-20 20:39:29 -0700

    Add scoreboard number

---
+++ MauiTest1/ScoreboardNumber.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Maui.Layouts;
+
+namespace MauiTest1
+{
+    public class ScoreboardNumber : AbsoluteLayout
+    {
+        public static readonly BindableProperty NumberProperty =
+            BindableProperty.Create(nameof(Number), typeof(int), typeof(ScoreboardNumber), 0);
+
+        //public int Number = 0;
+        public ScoreboardNumber()
+        {
+            BackgroundColor = Colors.Black;
+            Padding = 1;
+            HeightRequest = 23;
+            WidthRequest = 13;
+
+            Loaded += RenderNumber;
+            //SizeChanged += RenderNumber; // (?)
+        }
+
+        //public ScoreboardNumber(int number) : this()
+        //{
+        //    Number = number;
+        //}
+
+        public int Number
+        {
+            get { return (int)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        private void RenderNumber(object sender, EventArgs e)
+        {
+            if (Number < 0 || Number > 9)
+            {
+                return;
+            }
+
+            ScoreboardNumberCell.CellState topColor = ScoreboardNumberCell.CellState.RedDisabled;
+            ScoreboardNumberCell.CellState upperRightColor = ScoreboardNumberCell.CellState.RedDisabled;
+            ScoreboardNumberCell.CellState lowerRightColor = ScoreboardNumberCell.CellState.RedDisabled;
+            ScoreboardNumberCell.CellState bottomColor = ScoreboardNumberCell.CellState.RedDisabled;
+            ScoreboardNumberCell.CellState lowerLeftColor = ScoreboardNumberCell.CellState.RedDisabled;
+            ScoreboardNumberCell.CellState upperLeftColor = ScoreboardNumberCell.CellState.RedDisabled;
+            ScoreboardNumberCell.CellState centerColor = ScoreboardNumberCell.CellState.RedDisabled;
+
+            switch (Number)
+            {
+                case 0:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 1:
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 2:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 3:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 4:
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 5:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 6:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 7:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 8:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+                case 9:
+                default:
+                    topColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    lowerRightColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    bottomColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    upperLeftColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    centerColor = ScoreboardNumberCell.CellState.RedEnabled;
+                    break;
+            }
+
+            ScoreboardNumberCell top = new(ScoreboardNumberCell.CellOrientation.Top, topColor);
+            ScoreboardNumberCell upperRight = new(ScoreboardNumberCell.CellOrientation.Right, upperRightColor);
+            ScoreboardNumberCell lowerRight = new(ScoreboardNumberCell.CellOrientation.Right, lowerRightColor);
+            ScoreboardNumberCell bottom = new(ScoreboardNumberCell.CellOrientation.Bottom, bottomColor);
+            ScoreboardNumberCell lowerLeft = new(ScoreboardNumberCell.CellOrientation.Left, lowerLeftColor);
+            ScoreboardNumberCell upperLeft = new(ScoreboardNumberCell.CellOrientation.Left, upperLeftColor);
+            ScoreboardNumberCell center = new(ScoreboardNumberCell.CellOrientation.Center, centerColor);
+
+            Children.Clear();
+
+            Children.Add(top);
+            Children.Add(upperRight);
+            Children.Add(lowerRight);
+            Children.Add(bottom);
+            Children.Add(lowerLeft);
+            Children.Add(upperLeft);
+            Children.Add(center);
+
+            AbsoluteLayout.SetLayoutBounds(top, new Rect(1, 0, 9, 3));
+            AbsoluteLayout.SetLayoutBounds(upperRight, new Rect(8, 1, 3, 9));
+            AbsoluteLayout.SetLayoutBounds(lowerRight, new Rect(8, 11, 3, 9));
+            AbsoluteLayout.SetLayoutBounds(bottom, new Rect(1, 18, 9, 3));
+            AbsoluteLayout.SetLayoutBounds(lowerLeft, new Rect(0, 11, 3, 9));
+            AbsoluteLayout.SetLayoutBounds(upperLeft, new Rect(0, 1, 3, 9));
+            AbsoluteLayout.SetLayoutBounds(center, new Rect(1, 9, 9, 3));
+        }
+    }
+}
---
+++ MauiTest1/ScoreboardNumberCell.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using Path = Microsoft.Maui.Controls.Shapes.Path;
+
+namespace MauiTest1
+{
+    internal class ScoreboardNumberCell : AbsoluteLayout
+    {
+        public enum LineOrientation { Horizontal, Vertical };
+        public enum CellOrientation { Top, Bottom, Left, Right, Center };
+        public enum CellState { RedEnabled, RedDisabled };
+        public CellOrientation ThisOrientation = CellOrientation.Top;
+        public CellState ThisState = CellState.RedDisabled;
+
+        public ScoreboardNumberCell()
+        {
+        }
+
+        public ScoreboardNumberCell(CellOrientation orientation, CellState state)
+        {
+            ThisOrientation = orientation;
+            ThisState = state;
+
+            Loaded += CreateCell;
+            // SizeChanged += CreateCell; // (?)
+        }
+
+        private void CreateCell(object sender, EventArgs e)
+        {
+            LineOrientation orientation = LineOrientation.Horizontal;
+
+            string rowAColor = string.Empty;
+            string rowBColor = string.Empty;
+
+            if (ThisState == CellState.RedEnabled)
+            {
+                rowAColor = "#FFFF0000";
+                rowBColor = "#FFFF0000";
+            }
+            else
+            {
+                rowAColor = "#FF800000";
+                rowBColor = "#FF000000";
+            }
+
+            if (ThisOrientation == CellOrientation.Left || ThisOrientation == CellOrientation.Right)
+            {
+                HeightRequest = 9;
+                WidthRequest = 3;
+                orientation = LineOrientation.Vertical;
+            }
+            else
+            {
+                HeightRequest = 3;
+                WidthRequest = 9;
+            }
+
+            Path line1_1;
+            Path line1_2;
+            if (ThisOrientation == CellOrientation.Center)
+            {
+                line1_1 = CreateLine(5, 2, rowAColor, orientation);
+                line1_2 = CreateLine(3, 3, rowBColor, orientation);
+            }
+            else
+            {
+                line1_1 = CreateLine(9, 0, rowAColor, orientation);
+                line1_2 = CreateLine(7, 1, rowBColor, orientation);
+            }
+            Path line2_1 = CreateLine(7, 1, rowAColor, orientation);
+            Path line2_2 = CreateLine(5, 2, rowBColor, orientation);
+            Path line3_1 = CreateLine(5, 2, rowAColor, orientation);
+            Path line3_2 = CreateLine(3, 3, rowBColor, orientation);
+
+            Children.Add(line1_1);
+            Children.Add(line1_2);
+            Children.Add(line2_1);
+            Children.Add(line2_2);
+            Children.Add(line3_1);
+            Children.Add(line3_2);
+
+            int x1 = 0;
+            int x2 = 0;
+            int x3 = 0;
+            int y1 = 0;
+            int y2 = 0;
+            int y3 = 0;
+
+            if (ThisOrientation == CellOrientation.Top)
+            {
+                y2 = 1;
+                y3 = 2;
+            }
+            else if (ThisOrientation == CellOrientation.Bottom)
+            {
+                y1 = 2;
+                y2 = 1;
+            }
+            else if (ThisOrientation == CellOrientation.Left)
+            {
+                x2 = 1;
+                x3 = 2;
+            }
+            else if (ThisOrientation == CellOrientation.Right)
+            {
+                x1 = 2;
+                x2 = 1;
+            }
+            else if (ThisOrientation == CellOrientation.Center)
+            {
+                y2 = 1;
+                y3 = 2;
+            }
+
+            AbsoluteLayout.SetLayoutBounds(line1_1, new Rect(x1, y1, WidthRequest, HeightRequest));
+            AbsoluteLayout.SetLayoutBounds(line1_2, new Rect(x1, y1, WidthRequest, HeightRequest));
+            AbsoluteLayout.SetLayoutBounds(line2_1, new Rect(x2, y2, WidthRequest, HeightRequest));
+            AbsoluteLayout.SetLayoutBounds(line2_2, new Rect(x2, y2, WidthRequest, HeightRequest));
+            AbsoluteLayout.SetLayoutBounds(line3_1, new Rect(x3, y3, WidthRequest, HeightRequest));
+            AbsoluteLayout.SetLayoutBounds(line3_2, new Rect(x3, y3, WidthRequest, HeightRequest));
+
+        }
+
+        private Path CreateLine(
+            int size = 3,
+            int offset = 0,
+            string color = "#FFFF0000",
+            LineOrientation orientation = LineOrientation.Horizontal)
+        {
+            int verticalOffset = 0;
+            int horizontalOffset = 0;
+            int verticalPosition = 0;
+            int horizontalPosition = 0;
+
+            GeometryGroup geometryGroup = new();
+
+            if (orientation == LineOrientation.Horizontal)
+            {
+                horizontalOffset = offset;
+            } 
+            else
+            {
+                verticalOffset = offset;
+            }
+
+            for (int i = 0; i < size; i += 2)
+            {
+                if (orientation == LineOrientation.Horizontal)
+                {
+                    horizontalPosition = i;
+                }
+                else
+                {
+                    verticalPosition = i;
+                }
+                RectangleGeometry newRect = new();
+                newRect.Rect = new Rect(horizontalPosition, verticalPosition, 1, 1);
+                geometryGroup.Children.Add(newRect);
+            }
+
+            Path path = new();
+            path.Fill = Color.FromArgb(color);
+            path.StrokeThickness = 0;
+            path.Margin = new Thickness(horizontalOffset, verticalOffset, 0, 0);
+
+            path.Data = geometryGroup;
+
+            return path;
+        }
+    }
+}
